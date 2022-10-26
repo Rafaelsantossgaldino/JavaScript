ae6522836e0dc1dd3069ce70dc31835a17d4fd6b
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-26 20:41:10 -0300

    Switch

---
+++ part5/aula1.js
@@ -0,0 +1,28 @@
+let velocity = 90
+
+switch (velocity) {
+  case 80:
+    console.log("Sua valocidade é 100km/s")  
+    break;
+  case 160:
+    console.log("Sua velocidade é 160km/s")
+    break;
+  default:
+    console.log("Velocidade nao identificada")
+}
+
+let spaceship = "Elemental"
+
+switch (spaceship) {
+  case "Golais":
+    console.log("Nave mais resistente")
+    break;
+  case "Raptor":
+   console.log("Nave mais rapida")
+   break;
+  case "Enterprise":
+    console.log("Nave da frota estelar")
+    break;
+  default:
+  console.log("Nave comum")
+}
