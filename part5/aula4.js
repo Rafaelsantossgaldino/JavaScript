27cf6af29a56e5f108b947e22a34bc5539933de6
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-10-27 13:56:54 -0300

    funcoes simplificadas

---
+++ part5/aula4.js
@@ -0,0 +1,12 @@
+// Arrow  function normal e simplificada
+
+// Normal
+const doubleVelocity = (velocity) => {
+  return velocity * 2
+}
+
+// Simplificada
+const doubleSpeed = (velocity) => velocity * 2
+
+console.log(doubleVelocity(60))
+console.log(doubleSpeed(10))
