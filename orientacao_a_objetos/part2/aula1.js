5a683edd88f9c2ce5469bef0ad8a97c35eba8dc6
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-11-09 01:39:58 -0300

    um pouco mais sobre POO

---
+++ orientacao_a_objetos/part2/aula1.js
@@ -0,0 +1,15 @@
+// POO criando obj
+class SpaceStation {
+ constructor(name, platform) {
+  this.name = name;
+  this.platform = platform
+ }  
+}
+
+let resultSpacetation = new SpaceStation("F-16", 40)
+let darwin = new SpaceStation("Darwin")
+let helmt = new SpaceStation("Helmt", "Batalha")
+
+console.log(resultSpacetation)
+console.log(darwin)
+console.log(helmt)
---
+++ orientacao_a_objetos/part2/aula2.js
@@ -0,0 +1,15 @@
+class Spaceship{
+  constructor(name){
+    this.name = name;
+    this.velocity = 0;
+  }
+
+  speedUp(aceletare){ // Metodo
+    this.velocity += aceletare
+  }
+}
+
+let artemis = new Spaceship("Atemisr")
+artemis.speedUp(10)
+artemis.speedUp(15)
+console.log(artemis)
