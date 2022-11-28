7ac8fe34e827495a2c4b216c736796a1cb4209d6
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-11-28 20:39:44 -0300

    configuração inicial do servidor

---
+++ inicio_typescript/src/server.ts
@@ -0,0 +1,10 @@
+import express, { Request, Response } from "express";
+
+const server = express()
+
+server.get('/', (req: Request, res: Response) => {
+  res.send('Ola Mundo')
+})
+
+
+server.listen(3000)
