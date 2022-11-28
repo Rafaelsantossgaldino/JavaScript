cf775d0717bdf3f65f7f95cf08847137a80a1f58
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-11-28 20:39:21 -0300

    remove

--- inicio_typescript/src/index.ts
+++
@@ -1,8 +0,0 @@
-import Matematica from './Matematica'
-
-let n1: number = 10
-let n2: number = 20
-
-console.log(`SOMA: ${Matematica.somar(n1, n2)}`)
-console.log(`SUBTRAÇÃO: ${Matematica.subtrair(n1, n2)}`)
-console.log(`MULTIPLICAÇÃO: ${Matematica.multiplicar(n1, n2)}`)
