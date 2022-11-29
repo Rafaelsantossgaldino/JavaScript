4a253e22d15092e08dc39ff6fda075db2e197aae
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-11-28 21:32:33 -0300

    adoicionando  pagina nao encontrada

--- inicio_typescript/src/server.ts
+++ inicio_typescript/src/server.ts
@@ -1,12 +1,15 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import mainRoutes from  "./routes/index"
 import painelRoutes from "./routes/painel"
 
 const server = express()
+
+
 server.use(mainRoutes)
 server.use('/painel', painelRoutes)
-
-
+server.use((req: Request, res:Response) => {
+  res.status(404).send('Pagina não escontrada')
+})
 
 
 
