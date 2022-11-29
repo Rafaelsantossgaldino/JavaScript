3529640ca2b19dc14d3d27bb0019a53c54605fc9
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-11-28 21:27:22 -0300

    refactor

--- inicio_typescript/src/server.ts
+++ inicio_typescript/src/server.ts
@@ -1,10 +1,13 @@
-import express, { Request, Response } from "express";
+import express from "express";
+import mainRoutes from  "./routes/index"
+import painelRoutes from "./routes/painel"
 
 const server = express()
+server.use(mainRoutes)
+server.use('/painel', painelRoutes)
 
-server.get('/', (req: Request, res: Response) => {
-  res.send('Ola Mundo')
-})
+
+
 
 
 server.listen(3000)
