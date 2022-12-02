df13f7e880f46d4e7e25a9d01898acca60f327a6
Rafaelsantossgaldino <rafaelsantossgaldino2@gmail.com>
2022-12-02 20:17:46 -0300

    modificação de rota

--- inicio_typescript/src/routes/index.ts
+++ inicio_typescript/src/routes/index.ts
@@ -3,7 +3,7 @@ import { Router, Request, Response } from "express";
 const router =  Router();
 
 router.get('/', (req: Request, res: Response) => {
-  res.send('Home do site')
+ res.render('home')
 })
 
 router.get('/contatos', (req: Request, res: Response) => {
